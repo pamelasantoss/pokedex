7c9cbe600e52c116fae08c7591a3714d214610de
Pamela Santos <pamesantoss@hotmail.com>
2025-01-04 17:33:01 -0300

    feat: creating layout for sign in page

--- src/pages/_layouts/AuthLayout.tsx
+++ src/pages/_layouts/AuthLayout.tsx
@@ -1,11 +1,20 @@
+import { Origami } from "lucide-react"
 import { Outlet } from "react-router-dom"
 
 export function AuthLayout() {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="grid min-h-screen grid-cols-2 antialiased">
+      <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
+        <div className="flex items-center gap-3 text-lg text-foreground">
+          <Origami className="h-5 w-5" />
+          <span className="font-semibold">Pokedex</span>
+        </div>
+        <footer className="text-sm">
+          Pokemon search &copy; Pokedex - {new Date().getFullYear()}
+        </footer>
+      </div>
 
-      <div>
+      <div className="relative flex flex-col items-center justify-center">
         <Outlet />
       </div>
     </div>
--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -1,10 +1,40 @@
 import { Helmet } from "react-helmet-async"
+import { Label } from "../../components/ui/label"
+import { Input } from "../../components/ui/input"
+import { Button } from "../../components/ui/button"
 
 export function SignIn() {
   return (
     <>
       <Helmet title="Login" />
-      <h1>SignIn page</h1>
+      <div className="p-8">
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Access the dashboard
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Search for your favorite pokemons
+            </p>
+          </div>
+
+          <form className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Your e-mail</Label>
+              <Input id="email" type="email" />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="email">Your password</Label>
+              <Input id="password" type="password" />
+            </div>
+
+            <Button type="submit" className="w-full">
+              Sign in
+            </Button>
+          </form>
+        </div>
+      </div>
     </>
   )
 }
